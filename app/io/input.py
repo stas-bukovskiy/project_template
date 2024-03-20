1228aa27b2c6f2db15fcb9a645dcddd287e2666e
stas-bk <s.bukovskyi@aumify.io>
2024-03-20 19:33:10 +0200

    implement input/output stub function

---
+++ app/io/input.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+
+def input_from_console():
+    """
+    Function to input text from the console.
+
+    Returns:
+        str: The input text from the user.
+    """
+    pass
+
+
+def read_from_file_builtin(filename):
+    """
+    Function to read from a file using built-in funcs.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        str: The content of the file.
+    """
+    pass
+
+
+def read_from_file_pandas(filename):
+    """
+    Function to read from file using the pandas library.
+
+    Args:
+        filename (str): The name of the file to be read. The file should be in CSV format.
+
+    Returns:
+        list: The content of the file.
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,19 @@
+def output_to_console(text):
+    """
+    Function to output text to the console.
+
+    Args:
+        text (str): The text to be printed.
+    """
+    pass
+
+
+def write_to_file_builtin(filename, content):
+    """
+    Function to write content to a file using built-in funcs.
+
+    Args:
+        filename (str): The name of the file where the content will be written.
+        content (str): The content to be written to the file.
+    """
+    pass
